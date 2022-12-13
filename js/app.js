9d1a11e3dce06b0178fd86cd919df23c02b2855f
Vincent Auger <vincent.auger@gmail.com>
2022-12-12 21:51:05 -0400

    parallax fixed

--- js/app.js
+++ js/app.js
@@ -16,26 +16,26 @@ ScrollTrigger.create({
 
 
 // a function that calculate distance from absolute top of page to top of given middle of the given id
-function getOffsetTop(id) {
-    const el = document.getElementById(id);
-    const rect = el.getBoundingClientRect();
-    //console.log(rect.top);
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let offset = rect.top + scrollTop;
-    return offset + rect.height / 2;
-}
+// function getOffsetTop(id) {
+//     const el = document.getElementById(id);
+//     const rect = el.getBoundingClientRect();
+//     //console.log(rect.top);
+//     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     let offset = rect.top + scrollTop;
+//     return offset + rect.height / 2;
+// }
 
-function alignHeroMask() {
-    heroImgOffset = getOffsetTop("vt-hero-img");
-    heroMaskOffset = getOffsetTop("vt-hero-mask");
-    //console.log(heroImgOffset, heroMaskOffset)
-    let offset = heroImgOffset - heroMaskOffset;
-    const heroMask = document.getElementById("vt-hero-mask");
-    heroMask.style.top = `${offset}px`;
+// function alignHeroMask() {
+//     heroImgOffset = getOffsetTop("vt-hero-img");
+//     heroMaskOffset = getOffsetTop("vt-hero-mask");
+//     //console.log(heroImgOffset, heroMaskOffset)
+//     let offset = heroImgOffset - heroMaskOffset;
+//     const heroMask = document.getElementById("vt-hero-mask");
+//     heroMask.style.top = `${offset}px`;
 
-}
+// }
 
-alignHeroMask();
+// alignHeroMask();
 
 gsap.to(".vt-intro-background",
     {
@@ -46,7 +46,21 @@ gsap.to(".vt-intro-background",
             scrub: true,
             markers: false
         },
+        // bring the top position back to 0
         y: () => window.innerWidth > 992 ? "75vh" : "90vh",
+        ease: "none",
+    });
+
+gsap.to(".vt-intro-background",
+    {
+        scrollTrigger: {
+            trigger: ".down-arrow",
+            start: "top center",
+            end: "bottom top",
+            scrub: true,
+            markers: true
+        },
+        marginBottom: "40rem",
         ease: "none",
     });
 
